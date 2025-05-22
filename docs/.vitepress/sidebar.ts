87ac42ec63fa5ff964553a4832cccfb6babd8b9e
shouyu <wzxuvgyq@outlook.com>
2025-05-22 12:00:48 +0800

    更新文章

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -78,64 +78,144 @@ export const sidebar = {
               link: "/blogs/algorithm-notes/",
             },
             {
-              text: "数组理论基础",
-              link: "/blogs/algorithm-notes/数组理论基础",
+              text: "数组",
+              collapsed: true,
+              items: [
+                {
+                  text: "数组专题概览",
+                  link: "/blogs/algorithm-notes/array/",
+                },
+                {
+                  text: "数组理论基础",
+                  link: "/blogs/algorithm-notes/array/数组理论基础",
+                },
+                {
+                  text: "704 二分查找",
+                  link: "/blogs/algorithm-notes/array/代码随想录 数组 704 二分查找",
+                },
+                {
+                  text: "27 移除元素",
+                  link: "/blogs/algorithm-notes/array/代码随想录 数组 27 移除元素",
+                },
+                {
+                  text: "997 有序数组的平方",
+                  link: "/blogs/algorithm-notes/array/代码随想录 数组 997  有序数组的平方",
+                },
+                {
+                  text: "209 长度最小的子数组",
+                  link: "/blogs/algorithm-notes/array/代码随想录 数组 209 长度最小的子数组",
+                },
+                {
+                  text: "59 螺旋矩阵",
+                  link: "/blogs/algorithm-notes/array/代码随想录 数组 59 螺旋矩阵",
+                },
+                {
+                  text: "54 区间和",
+                  link: "/blogs/algorithm-notes/array/代码随想录 数组 54 区间和",
+                },
+                {
+                  text: "44 开发商购买土地",
+                  link: "/blogs/algorithm-notes/array/代码随想录 数组 44 开发商购买土地",
+                },
+              ],
             },
             {
-              text: "704. 二分查找",
-              link: "/blogs/algorithm-notes/代码随想录 704. 二分查找",
+              text: "链表",
+              collapsed: true,
+              items: [
+                {
+                  text: "链表专题概览",
+                  link: "/blogs/algorithm-notes/linkedlist/",
+                },
+                {
+                  text: "203 移除链表元素",
+                  link: "/blogs/algorithm-notes/linkedlist/代码随想录 链表 203 移除链表元素",
+                },
+                {
+                  text: "707 设计链表",
+                  link: "/blogs/algorithm-notes/linkedlist/代码随想录 链表 707 设计链表",
+                },
+                {
+                  text: "206 翻转链表",
+                  link: "/blogs/algorithm-notes/linkedlist/代码随想录 链表 206 翻转链表",
+                },
+                {
+                  text: "24 两两交换链表中的节点",
+                  link: "/blogs/algorithm-notes/linkedlist/代码随想录 链表 24 两两交换链表中的节点",
+                },
+                {
+                  text: "19 删除链表的倒数第N个节点",
+                  link: "/blogs/algorithm-notes/linkedlist/代码随想录 链表 19 删除链表的倒数第N个节点",
+                },
+                {
+                  text: "160 链表相交",
+                  link: "/blogs/algorithm-notes/linkedlist/代码随想录 链表 160 链表相交",
+                },
+                {
+                  text: "142 环形链表",
+                  link: "/blogs/algorithm-notes/linkedlist/代码随想录 链表 142 环形链表",
+                },
+              ],
             },
             {
-              text: "27. 移除元素",
-              link: "/blogs/algorithm-notes/代码随想录 数组 27 移除元素",
+              text: "哈希表",
+              collapsed: true,
+              items: [
+                {
+                  text: "哈希表专题概览",
+                  link: "/blogs/algorithm-notes/hash/",
+                },
+                {
+                  text: "242 有效的字母异位词",
+                  link: "/blogs/algorithm-notes/hash/代码随想录 哈希表 242 字母的有效异位词",
+                },
+                {
+                  text: "349 两个数组的交集",
+                  link: "/blogs/algorithm-notes/hash/代码随想录 哈希表 349 两个数组的交集",
+                },
+                {
+                  text: "202 快乐数",
+                  link: "/blogs/algorithm-notes/hash/代码随想录 哈希表 202 快乐数",
+                },
+                {
+                  text: "1 两数之和",
+                  link: "/blogs/algorithm-notes/hash/代码随想录 哈希表 1 两数之和",
+                },
+                {
+                  text: "454 四数相加II",
+                  link: "/blogs/algorithm-notes/hash/代码随想录 哈希表 454 四数相加",
+                },
+                {
+                  text: "383 赎金信",
+                  link: "/blogs/algorithm-notes/hash/代码随想录 哈希表 383 赎金信",
+                },
+                {
+                  text: "15 三数之和",
+                  link: "/blogs/algorithm-notes/hash/代码随想录 哈希表 15 三数之和",
+                },
+              ],
             },
             {
-              text: "997. 有序数组的平方",
-              link: "/blogs/algorithm-notes/代码随想录 数组：997. 有序数组的平方",
-            },
-            {
-              text: "209. 长度最小的子数组",
-              link: "/blogs/algorithm-notes/代码随想录 数组 209.长度最小的子数组",
-            },
-            {
-              text: "59. 螺旋矩阵",
-              link: "/blogs/algorithm-notes/代码随想录 数组 59 螺旋矩阵",
-            },
-            {
-              text: "54. 区间和",
-              link: "/blogs/algorithm-notes/代码随想录 数组 54区间和",
-            },
-            {
-              text: "44. 开发商购买土地",
-              link: "/blogs/algorithm-notes/代码随想录 数组 44 开发商购买土地",
-            },
-            {
-              text: "203. 移除链表元素",
-              link: "/blogs/algorithm-notes/代码随想录 链表 203 移除链表元素",
-            },
-            {
-              text: "707. 设计链表",
-              link: "/blogs/algorithm-notes/代码随想录 链表 707 设计链表",
-            },
-            {
-              text: "206. 翻转链表",
-              link: "/blogs/algorithm-notes/代码随想录 链表 206 翻转链表",
-            },
-            {
-              text: "24. 两两交换链表中的节点",
-              link: "/blogs/algorithm-notes/代码随想录 链表 24 两两交换链表中的节点",
-            },
-            {
-              text: "19. 删除链表的倒数第N个节点",
-              link: "/blogs/algorithm-notes/代码随想录 链表 19 删除链表的倒数第N个节点.",
-            },
-            {
-              text: "160. 链表相交",
-              link: "/blogs/algorithm-notes/代码随想录 链表 160 链表相交",
-            },
-            {
-              text: "140. 环形链表",
-              link: "/blogs/algorithm-notes/代码随想录 链表 140 环形链表",
+              text: "字符串",
+              collapsed: true,
+              items: [
+                {
+                  text: "字符串专题概览",
+                  link: "/blogs/algorithm-notes/string/",
+                },
+                {
+                  text: "344 反转字符串",
+                  link: "/blogs/algorithm-notes/string/代码随想录 字符串 344 反转字符串",
+                },
+                {
+                  text: "541 反转字符串II",
+                  link: "/blogs/algorithm-notes/string/代码随想录 字符串 541 反转字符串 II",
+                },
+                {
+                  text: "替换数字",
+                  link: "/blogs/algorithm-notes/string/代码随想录 字符串 kama  替换数字",
+                },
+              ],
             },
           ],
         },
