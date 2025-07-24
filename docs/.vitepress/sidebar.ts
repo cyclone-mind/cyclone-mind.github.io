333730ecb704943e1e7a092a0d515947f89697dd
shouyu <wzxuvgyq@outlook.com>
2025-07-24 19:07:17 +0800

    Refactor algorithm notes structure and sidebar
    Standardized file and link naming for algorithm notes by removing redundant prefixes and numbers from filenames and sidebar links. Updated sidebar.ts and index.md files for array, linked list, hash, and string sections to use simplified names. Added new sections and entries for stack-queue, binary-tree, and backtracking topics, and updated the reference section. This improves maintainability and navigation consistency across the documentation.

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -90,32 +90,32 @@ export const sidebar = {
                   link: "/blogs/algorithm-notes/array/数组理论基础",
                 },
                 {
-                  text: "704 二分查找",
-                  link: "/blogs/algorithm-notes/array/代码随想录 数组 704 二分查找",
+                  text: "二分查找",
+                  link: "/blogs/algorithm-notes/array/二分查找",
                 },
                 {
-                  text: "27 移除元素",
-                  link: "/blogs/algorithm-notes/array/代码随想录 数组 27 移除元素",
+                  text: "移除元素",
+                  link: "/blogs/algorithm-notes/array/移除元素",
                 },
                 {
-                  text: "997 有序数组的平方",
-                  link: "/blogs/algorithm-notes/array/代码随想录 数组 997  有序数组的平方",
+                  text: "有序数组的平方",
+                  link: "/blogs/algorithm-notes/array/有序数组的平方",
                 },
                 {
-                  text: "209 长度最小的子数组",
-                  link: "/blogs/algorithm-notes/array/代码随想录 数组 209 长度最小的子数组",
+                  text: "长度最小的子数组",
+                  link: "/blogs/algorithm-notes/array/长度最小的子数组",
                 },
                 {
-                  text: "59 螺旋矩阵",
-                  link: "/blogs/algorithm-notes/array/代码随想录 数组 59 螺旋矩阵",
+                  text: "螺旋矩阵",
+                  link: "/blogs/algorithm-notes/array/螺旋矩阵",
                 },
                 {
-                  text: "54 区间和",
-                  link: "/blogs/algorithm-notes/array/代码随想录 数组 54 区间和",
+                  text: "区间和",
+                  link: "/blogs/algorithm-notes/array/区间和",
                 },
                 {
-                  text: "44 开发商购买土地",
-                  link: "/blogs/algorithm-notes/array/代码随想录 数组 44 开发商购买土地",
+                  text: "开发商购买土地",
+                  link: "/blogs/algorithm-notes/array/开发商购买土地",
                 },
               ],
             },
@@ -128,32 +128,32 @@ export const sidebar = {
                   link: "/blogs/algorithm-notes/linkedlist/",
                 },
                 {
-                  text: "203 移除链表元素",
-                  link: "/blogs/algorithm-notes/linkedlist/代码随想录 链表 203 移除链表元素",
+                  text: "移除链表元素",
+                  link: "/blogs/algorithm-notes/linkedlist/移除链表元素",
                 },
                 {
-                  text: "707 设计链表",
-                  link: "/blogs/algorithm-notes/linkedlist/代码随想录 链表 707 设计链表",
+                  text: "设计链表",
+                  link: "/blogs/algorithm-notes/linkedlist/设计链表",
                 },
                 {
-                  text: "206 翻转链表",
-                  link: "/blogs/algorithm-notes/linkedlist/代码随想录 链表 206 翻转链表",
+                  text: "翻转链表",
+                  link: "/blogs/algorithm-notes/linkedlist/翻转链表",
                 },
                 {
-                  text: "24 两两交换链表中的节点",
-                  link: "/blogs/algorithm-notes/linkedlist/代码随想录 链表 24 两两交换链表中的节点",
+                  text: "两两交换链表中的节点",
+                  link: "/blogs/algorithm-notes/linkedlist/两两交换链表中的节点",
                 },
                 {
-                  text: "19 删除链表的倒数第N个节点",
-                  link: "/blogs/algorithm-notes/linkedlist/代码随想录 链表 19 删除链表的倒数第N个节点",
+                  text: "删除链表的倒数第N个节点",
+                  link: "/blogs/algorithm-notes/linkedlist/删除链表的倒数第N个节点",
                 },
                 {
-                  text: "160 链表相交",
-                  link: "/blogs/algorithm-notes/linkedlist/代码随想录 链表 160 链表相交",
+                  text: "链表相交",
+                  link: "/blogs/algorithm-notes/linkedlist/链表相交",
                 },
                 {
-                  text: "142 环形链表",
-                  link: "/blogs/algorithm-notes/linkedlist/代码随想录 链表 142 环形链表",
+                  text: "环形链表",
+                  link: "/blogs/algorithm-notes/linkedlist/环形链表",
                 },
               ],
             },
@@ -166,32 +166,36 @@ export const sidebar = {
                   link: "/blogs/algorithm-notes/hash/",
                 },
                 {
-                  text: "242 有效的字母异位词",
-                  link: "/blogs/algorithm-notes/hash/代码随想录 哈希表 242 字母的有效异位词",
+                  text: "有效的字母异位词",
+                  link: "/blogs/algorithm-notes/hash/有效的字母异位词",
                 },
                 {
-                  text: "349 两个数组的交集",
-                  link: "/blogs/algorithm-notes/hash/代码随想录 哈希表 349 两个数组的交集",
+                  text: "两个数组的交集",
+                  link: "/blogs/algorithm-notes/hash/两个数组的交集",
                 },
                 {
-                  text: "202 快乐数",
-                  link: "/blogs/algorithm-notes/hash/代码随想录 哈希表 202 快乐数",
+                  text: "快乐数",
+                  link: "/blogs/algorithm-notes/hash/快乐数",
                 },
                 {
-                  text: "1 两数之和",
-                  link: "/blogs/algorithm-notes/hash/代码随想录 哈希表 1 两数之和",
+                  text: "两数之和",
+                  link: "/blogs/algorithm-notes/hash/两数之和",
                 },
                 {
-                  text: "454 四数相加II",
-                  link: "/blogs/algorithm-notes/hash/代码随想录 哈希表 454 四数相加",
+                  text: "四数相加II",
+                  link: "/blogs/algorithm-notes/hash/四数相加II",
                 },
                 {
-                  text: "383 赎金信",
-                  link: "/blogs/algorithm-notes/hash/代码随想录 哈希表 383 赎金信",
+                  text: "赎金信",
+                  link: "/blogs/algorithm-notes/hash/赎金信",
                 },
                 {
-                  text: "15 三数之和",
-                  link: "/blogs/algorithm-notes/hash/代码随想录 哈希表 15 三数之和",
+                  text: "三数之和",
+                  link: "/blogs/algorithm-notes/hash/三数之和",
+                },
+                {
+                  text: "四数之和",
+                  link: "/blogs/algorithm-notes/hash/四数之和",
                 },
               ],
             },
@@ -204,16 +208,266 @@ export const sidebar = {
                   link: "/blogs/algorithm-notes/string/",
                 },
                 {
-                  text: "344 反转字符串",
-                  link: "/blogs/algorithm-notes/string/代码随想录 字符串 344 反转字符串",
+                  text: "反转字符串",
+                  link: "/blogs/algorithm-notes/string/反转字符串",
                 },
                 {
-                  text: "541 反转字符串II",
-                  link: "/blogs/algorithm-notes/string/代码随想录 字符串 541 反转字符串 II",
+                  text: "反转字符串II",
+                  link: "/blogs/algorithm-notes/string/反转字符串II",
                 },
                 {
                   text: "替换数字",
-                  link: "/blogs/algorithm-notes/string/代码随想录 字符串 kama  替换数字",
+                  link: "/blogs/algorithm-notes/string/替换数字",
+                },
+              ],
+            },
+            {
+              text: "栈与队列",
+              collapsed: true,
+              items: [
+                {
+                  text: "理论基础",
+                  link: "/blogs/algorithm-notes/stack-queue/理论基础",
+                },
+                {
+                  text: "栈实现队列",
+                  link: "/blogs/algorithm-notes/stack-queue/栈实现队列",
+                },
+                {
+                  text: "队列实现栈",
+                  link: "/blogs/algorithm-notes/stack-queue/队列实现栈",
+                },
+                {
+                  text: "有效的括号",
+                  link: "/blogs/algorithm-notes/stack-queue/有效的括号",
+                },
+                {
+                  text: "删除字符串中所有相邻重复项",
+                  link: "/blogs/algorithm-notes/stack-queue/删除字符串中所有相邻重复项",
+                },
+                {
+                  text: "逆波兰表达式",
+                  link: "/blogs/algorithm-notes/stack-queue/逆波兰表达式.",
+                },
+                {
+                  text: "滑动窗口最大值",
+                  link: "/blogs/algorithm-notes/stack-queue/滑动窗口最大值",
+                },
+                {
+                  text: "前K个高频元素",
+                  link: "/blogs/algorithm-notes/stack-queue/前K个高频元素",
+                },
+                {
+                  text: "总结",
+                  link: "/blogs/algorithm-notes/stack-queue/总结",
+                },
+              ],
+            },
+            {
+              text: "二叉树",
+              collapsed: true,
+              items: [
+                {
+                  text: "理论基础",
+                  link: "/blogs/algorithm-notes/binary-tree/理论基础",
+                },
+                {
+                  text: "递归遍历",
+                  link: "/blogs/algorithm-notes/binary-tree/递归遍历",
+                },
+                {
+                  text: "迭代遍历",
+                  link: "/blogs/algorithm-notes/binary-tree/迭代遍历",
+                },
+                {
+                  text: "统一迭代",
+                  link: "/blogs/algorithm-notes/binary-tree/统一迭代",
+                },
+                {
+                  text: "层序遍历",
+                  link: "/blogs/algorithm-notes/binary-tree/层序遍历",
+                },
+                {
+                  text: "翻转二叉树",
+                  link: "/blogs/algorithm-notes/binary-tree/翻转二叉树",
+                },
+                {
+                  text: "对称二叉树",
+                  link: "/blogs/algorithm-notes/binary-tree/对称二叉树",
+                },
+                {
+                  text: "最大深度",
+                  link: "/blogs/algorithm-notes/binary-tree/最大深度",
+                },
+                {
+                  text: "最小深度",
+                  link: "/blogs/algorithm-notes/binary-tree/最小深度",
+                },
+                {
+                  text: "完全二叉树节点个数",
+                  link: "/blogs/algorithm-notes/binary-tree/完全二叉树节点个数",
+                },
+                {
+                  text: "平衡二叉树",
+                  link: "/blogs/algorithm-notes/binary-tree/平衡二叉树",
+                },
+                {
+                  text: "所有路径",
+                  link: "/blogs/algorithm-notes/binary-tree/所有路径",
+                },
+                {
+                  text: "左叶子之和",
+                  link: "/blogs/algorithm-notes/binary-tree/左叶子之和",
+                },
+                {
+                  text: "左下角的值",
+                  link: "/blogs/algorithm-notes/binary-tree/左下角的值",
+                },
+                {
+                  text: "路径总和",
+                  link: "/blogs/algorithm-notes/binary-tree/路径总和",
+                },
+                {
+                  text: "从中序遍历和后序遍历构造二叉树",
+                  link: "/blogs/algorithm-notes/binary-tree/从中序遍历和后序遍历构造二叉树",
+                },
+                {
+                  text: "最大二叉树",
+                  link: "/blogs/algorithm-notes/binary-tree/最大二叉树",
+                },
+                {
+                  text: "合并二叉树",
+                  link: "/blogs/algorithm-notes/binary-tree/合并二叉树",
+                },
+                {
+                  text: "二叉搜索树中的搜索",
+                  link: "/blogs/algorithm-notes/binary-tree/二叉搜索树中的搜索",
+                },
+                {
+                  text: "验证二叉搜索树",
+                  link: "/blogs/algorithm-notes/binary-tree/验证二叉搜索树",
+                },
+                {
+                  text: "二叉搜索树的最小绝对差",
+                  link: "/blogs/algorithm-notes/binary-tree/二叉搜索树的最小绝对差",
+                },
+                {
+                  text: "二叉搜索树中的众数",
+                  link: "/blogs/algorithm-notes/binary-tree/二叉搜索树中的众数",
+                },
+                {
+                  text: "二叉树的最近公共祖先",
+                  link: "/blogs/algorithm-notes/binary-tree/二叉树的最近公共祖先",
+                },
+                {
+                  text: "二叉搜索树的最近公共祖先",
+                  link: "/blogs/algorithm-notes/binary-tree/二叉搜索树的最近公共祖先",
+                },
+                {
+                  text: "二叉搜索树中的插入",
+                  link: "/blogs/algorithm-notes/binary-tree/二叉搜索树中的插入",
+                },
+                {
+                  text: "二叉搜索树的删除",
+                  link: "/blogs/algorithm-notes/binary-tree/二叉搜索树的删除",
+                },
+                {
+                  text: "二叉搜索树的修剪",
+                  link: "/blogs/algorithm-notes/binary-tree/二叉搜索树的修剪",
+                },
+                {
+                  text: "将有序数组转换为二叉搜索树",
+                  link: "/blogs/algorithm-notes/binary-tree/将有序数组转换为二叉搜索树",
+                },
+                {
+                  text: "二叉搜索树转换为累加树",
+                  link: "/blogs/algorithm-notes/binary-tree/二叉搜索树转换为累加树",
+                },
+                {
+                  text: "二叉树总结",
+                  link: "/blogs/algorithm-notes/binary-tree/二叉树总结",
+                },
+              ],
+            },
+            {
+              text: "回溯算法",
+              collapsed: true,
+              items: [
+                {
+                  text: "回溯理论基础",
+                  link: "/blogs/algorithm-notes/backtracking/回溯理论基础",
+                },
+                {
+                  text: "组合",
+                  link: "/blogs/algorithm-notes/backtracking/组合",
+                },
+                {
+                  text: "组合优化",
+                  link: "/blogs/algorithm-notes/backtracking/组合优化",
+                },
+                {
+                  text: "组合总和III",
+                  link: "/blogs/algorithm-notes/backtracking/组合总和III",
+                },
+                {
+                  text: "电话号码字母组合",
+                  link: "/blogs/algorithm-notes/backtracking/电话号码字母组合",
+                },
+                {
+                  text: "组合总和",
+                  link: "/blogs/algorithm-notes/backtracking/组合总和",
+                },
+                {
+                  text: "组合总和II",
+                  link: "/blogs/algorithm-notes/backtracking/组合总和II",
+                },
+                {
+                  text: "分割回文串",
+                  link: "/blogs/algorithm-notes/backtracking/分割回文串",
+                },
+                {
+                  text: "复原IP地址",
+                  link: "/blogs/algorithm-notes/backtracking/复原IP地址",
+                },
+                {
+                  text: "子集",
+                  link: "/blogs/algorithm-notes/backtracking/子集",
+                },
+                {
+                  text: "子集II",
+                  link: "/blogs/algorithm-notes/backtracking/子集II",
+                },
+                {
+                  text: "递增子序列",
+                  link: "/blogs/algorithm-notes/backtracking/递增子序列",
+                },
+                {
+                  text: "全排列",
+                  link: "/blogs/algorithm-notes/backtracking/全排列",
+                },
+                {
+                  text: "全排列II",
+                  link: "/blogs/algorithm-notes/backtracking/全排列II",
+                },
+                {
+                  text: "回溯算法去重另一种写法",
+                  link: "/blogs/algorithm-notes/backtracking/回溯算法去重另一种写法",
+                },
+                {
+                  text: "重新安排行程",
+                  link: "/blogs/algorithm-notes/backtracking/重新安排行程",
+                },
+                {
+                  text: "N皇后",
+                  link: "/blogs/algorithm-notes/backtracking/N皇后",
+                },
+                {
+                  text: "解数独",
+                  link: "/blogs/algorithm-notes/backtracking/解数独",
+                },
+                {
+                  text: "回溯总结",
+                  link: "/blogs/algorithm-notes/backtracking/回溯总结",
                 },
               ],
             },
@@ -250,6 +504,15 @@ export const sidebar = {
     {
       text: "影像作品",
       items: [{ text: "上海时刻", link: "/photos/shanghai-moments" }],
+    },
+  ],
+  "/reference/": [
+    {
+      text: "推荐",
+      items: [
+        { text: "学习资料推荐", link: "/reference/study-reference" },
+        { text: "使用效率工具", link: "/reference/multi-platform" },
+      ],
     },
   ],
 };
